5f3a9d0f6f86d11008949bb745c017ec8be76921
Ovsiannikov Andrey <andrey22ovs@yandex.ru>
2020-11-25 22:27:22 +0300

    Прямоугольник

--- pppppp.py
+++ pppppp.py
@@ -1,11 +1,12 @@
 import pygame
 
+x, y = input().split()
+
 
 def draw():
     screen.fill((0, 0, 0))
-    color = pygame.Color('#ffffff')
-    pygame.draw.line(screen, color, (0, 0), (width, height), 5)
-    pygame.draw.line(screen, color, (width, 0), (0, height), 5)
+    color = pygame.Color('#ff0000ff')
+    pygame.draw.rect(screen, color, (1, 1, width - 2, height - 2))
 
 
 if __name__ == '__main__':
@@ -13,7 +14,14 @@ if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Крест')
     # размеры окна:
-    size = width, height = 800, 600
+    assert '.' not in x and '.' not in y
+    try:
+        x = int(x)
+        y = int(y)
+    except ValueError:
+        raise ValueError('Это далеко не числа. Ты там прогу не попутал?!\n:)')
+    assert x < 1200 and y < 800
+    size = width, height = x, y
     # screen — холст, на котором нужно рисовать:
     screen = pygame.display.set_mode(size)
     # формирование кадра:
