64c1a5a88118f479bf0c9b33b9b376957d878af5
Ovsiannikov Andrey <andrey22ovs@yandex.ru>
2020-11-25 22:34:00 +0300

    Прямоугольник(norm)

--- pppppp.py
+++ pppppp.py
@@ -14,24 +14,30 @@ if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Крест')
     # размеры окна:
-    assert '.' not in x and '.' not in y
+    flag = True
     try:
+        assert '.' not in x and '.' not in y
         x = int(x)
         y = int(y)
+        assert x < 1200 and y < 800
+    except AssertionError:
+        print('Неправильный формат ввода')
+        flag = False
     except ValueError:
-        raise ValueError('Это далеко не числа. Ты там прогу не попутал?!\n:)')
-    assert x < 1200 and y < 800
-    size = width, height = x, y
-    # screen — холст, на котором нужно рисовать:
-    screen = pygame.display.set_mode(size)
-    # формирование кадра:
-    # команды рисования на холсте
-    draw()
-    # ...
-    # смена (отрисовка) кадра:
-    pygame.display.flip()
-    # ожидание закрытия окна:
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    # завершение работы:
+        print('Неправильный формат ввода')
+        flag = False
+    if flag:
+        size = width, height = x, y
+        # screen — холст, на котором нужно рисовать:
+        screen = pygame.display.set_mode(size)
+        # формирование кадра:
+        # команды рисования на холсте
+        draw()
+        # ...
+        # смена (отрисовка) кадра:
+        pygame.display.flip()
+        # ожидание закрытия окна:
+        while pygame.event.wait().type != pygame.QUIT:
+            pass
+        # завершение работы:
     pygame.quit()
