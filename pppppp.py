0ee6e259181ebfcfd38ef8948c199c74cbb9bab0
Ovsiannikov Andrey <andrey22ovs@yandex.ru>
2020-11-26 19:26:54 +0300

    Кирпичи

--- pppppp.py
+++ pppppp.py
@@ -1,49 +1,34 @@
 import pygame
 
-n = input()
 
-
-def draw_ci(p1, p2, p3, p4):
-    color = pygame.Color('orange')
-    pygame.draw.polygon(screen, color, (p1, p2, p3, p4))
+def draw_rect(x, y):
+    color = pygame.Color('red')
+    pygame.draw.rect(screen, color, (x, y, 30, 15))
 
 
 if __name__ == '__main__':
     # инициализация pygame:
     pygame.init()
-    pygame.display.set_caption('Ромбики')
+    pygame.display.set_caption('Кирпичи')
     # размеры окна:
-    flag = True
-    try:
-        assert '.' not in n
-        n = int(n)
-        assert n < 300
-    except AssertionError:
-        print('Неправильный формат ввода')
-        flag = False
-    except ValueError:
-        print('Неправильный формат ввода')
-        flag = False
-    if flag:
-        size = width, height = 300, 300
-        # screen — холст, на котором нужно рисовать:
-        screen = pygame.display.set_mode(size)
-        screen.fill(pygame.Color('yellow'))
-        # формирование кадра:
-        k = width // n
-        a = n // 2
-        x = 0
-        y = 0
-        for i in range(k):
-            for j in range(k):
-                draw_ci((x, y + a), (x + a, y), (x + 2 * a, y + a), (x + a, y + 2 * a))
-                x += a * 2
-            x = 0
-            y += a * 2
-        # смена (отрисовка) кадра:
-        pygame.display.flip()
-        # ожидание закрытия окна:
-        while pygame.event.wait().type != pygame.QUIT:
-            pass
-        # завершение работы:
+    size = width, height = 300, 200
+    # screen — холст, на котором нужно рисовать:
+    screen = pygame.display.set_mode(size)
+    screen.fill(pygame.Color('#ffffff'))
+    # формирование кадра:
+    x1 = 0
+    y1 = 0
+    for i in range(12):
+        for j in range(10):
+            draw_rect(x1 + j * 30 + 2 * j, y1 + i * 15 + 2 * i)
+        if not(i % 2):
+            x1 = -15
+        else:
+            x1 = 0
+    # смена (отрисовка) кадра:
+    pygame.display.flip()
+    # ожидание закрытия окна:
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    # завершение работы:
     pygame.quit()
