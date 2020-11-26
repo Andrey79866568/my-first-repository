99a4e951d4f990d3fb71c84860031180e3d073a4
Ovsiannikov Andrey <andrey22ovs@yandex.ru>
2020-11-26 15:31:31 +0300

    Сфера

--- pppppp.py
+++ pppppp.py
@@ -1,23 +1,22 @@
 import pygame
 
-w, n = input().split()
+n = input()
 
 
-def draw_ci(color, cent, radius):
-    pygame.draw.circle(screen, color, cent, radius)
+def draw_ci(x_st, y_st):
+    color = pygame.Color('#ffffff')
+    pygame.draw.ellipse(screen, color, (x_st, y_st, width - 2 * x_st, width - 2 * y_st), 1)
 
 
 if __name__ == '__main__':
     # инициализация Pygame:
     pygame.init()
-    pygame.display.set_caption('Мишень')
+    pygame.display.set_caption('Сфера')
     # размеры окна:
     flag = True
     try:
-        assert '.' not in w and '.' not in n
-        w = int(w)
+        assert '.' not in n
         n = int(n)
-        assert w * n * 2 < 800
     except AssertionError:
         print('Неправильный формат ввода')
         flag = False
@@ -25,17 +24,22 @@ if __name__ == '__main__':
         print('Неправильный формат ввода')
         flag = False
     if flag:
-        colors = ['red', 'green', 'blue']
-        size = width, height = w * n * 2, w * n * 2
-        center = w * n, w * n
-        rad = w * n
+        size = width, height = 300, 300
+        delt = (width / n) / 2
+        x = 0
+        y = 0
         # screen — холст, на котором нужно рисовать:
         screen = pygame.display.set_mode(size)
         screen.fill((0, 0, 0))
         # формирование кадра:
         # команды рисования на холсте
         for i in range(n):
-            draw_ci(colors[i % 3], center, rad - i * w)
+            draw_ci(x, y)
+            x += delt
+        x = 0
+        for i in range(n):
+            draw_ci(x, y)
+            y += delt
         # ...
         # смена (отрисовка) кадра:
         pygame.display.flip()
