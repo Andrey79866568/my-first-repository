8af3df68630c9c146db908a8d06d10d18be160de
Ovsiannikov Andrey <andrey22ovs@yandex.ru>
2020-11-26 19:01:08 +0300

    Ромбики

--- pppppp.py
+++ pppppp.py
@@ -3,20 +3,21 @@ import pygame
 n = input()
 
 
-def draw_ci(x_st, y_st):
-    color = pygame.Color('#ffffff')
-    pygame.draw.ellipse(screen, color, (x_st, y_st, width - 2 * x_st, width - 2 * y_st), 1)
+def draw_ci(p1, p2, p3, p4):
+    color = pygame.Color('orange')
+    pygame.draw.polygon(screen, color, (p1, p2, p3, p4))
 
 
 if __name__ == '__main__':
-    # инициализация Pygame:
+    # инициализация pygame:
     pygame.init()
-    pygame.display.set_caption('Сфера')
+    pygame.display.set_caption('Ромбики')
     # размеры окна:
     flag = True
     try:
         assert '.' not in n
         n = int(n)
+        assert n < 300
     except AssertionError:
         print('Неправильный формат ввода')
         flag = False
@@ -25,22 +26,20 @@ if __name__ == '__main__':
         flag = False
     if flag:
         size = width, height = 300, 300
-        delt = (width / n) / 2
-        x = 0
-        y = 0
         # screen — холст, на котором нужно рисовать:
         screen = pygame.display.set_mode(size)
-        screen.fill((0, 0, 0))
+        screen.fill(pygame.Color('yellow'))
         # формирование кадра:
-        # команды рисования на холсте
-        for i in range(n):
-            draw_ci(x, y)
-            x += delt
+        k = width // n
+        a = n // 2
         x = 0
-        for i in range(n):
-            draw_ci(x, y)
-            y += delt
-        # ...
+        y = 0
+        for i in range(k):
+            for j in range(k):
+                draw_ci((x, y + a), (x + a, y), (x + 2 * a, y + a), (x + a, y + 2 * a))
+                x += a * 2
+            x = 0
+            y += a * 2
         # смена (отрисовка) кадра:
         pygame.display.flip()
         # ожидание закрытия окна:
