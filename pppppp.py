fada5f20412d560d94218955951e31d524956fb1
Ovsiannikov Andrey <andrey22ovs@yandex.ru>
2020-11-28 21:05:45 +0300

    Шарики

--- pppppp.py
+++ pppppp.py
@@ -2,35 +2,47 @@ import pygame
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption('Движущийся круг 2')
-    size = width, height = 400, 400
+    pygame.display.set_caption('Решение')
+    size = width, height = 500, 500
     screen = pygame.display.set_mode(size)
-    screen.fill((0, 0, 255))
+    screen.fill((0, 0, 0))
     fps = 95  # количество кадров в секунду
-    v = 10
     clock = pygame.time.Clock()
+    rad = 10
+    v = 100
+    xy_list = []
     running = True
-    start = False
-    x, y = 0, 0
-    rad = 1
-    while running:  # главный игровой цикл
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONUP:
-                x, y = event.pos
-                rad = 1
-                screen.fill((0, 0, 255))
-                start = True
-        rad += v * clock.tick(fps) / 1000
-        if start:
-            pygame.draw.circle(screen, 'yellow', (x, y), rad)
-        # обработка остальных событий
-        # ...
-        # формирование кадра
-        # ...
-        pygame.display.flip()  # смена кадра
-        # изменение игрового мира
-        # ...
-        # временная задержка
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                xy_list.append([event.pos[0], event.pos[1], 'vl'])
+        time = clock.tick(fps)
+        for i in range(len(xy_list)):
+            pygame.draw.circle(screen, (255, 255, 255), (xy_list[i][0], xy_list[i][1]), rad)
+            if xy_list[i][2] == 'vl':
+                xy_list[i][0] -= v * time / 1000
+                xy_list[i][1] -= v * time / 1000
+            elif xy_list[i][2] == 'vr':
+                xy_list[i][0] += v * time / 1000
+                xy_list[i][1] -= v * time / 1000
+            elif xy_list[i][2] == 'dl':
+                xy_list[i][0] -= v * time / 1000
+                xy_list[i][1] += v * time / 1000
+            elif xy_list[i][2] == 'dr':
+                xy_list[i][0] += v * time / 1000
+                xy_list[i][1] += v * time / 1000
+
+            if xy_list[i][0] - rad < 2:
+                xy_list[i][2] = xy_list[i][2][0] + 'r'
+            elif xy_list[i][1] - rad < 2:
+                xy_list[i][2] = 'd' + xy_list[i][2][1]
+
+            elif width - xy_list[i][0] - rad < 2:
+                xy_list[i][2] = xy_list[i][2][0] + 'l'
+            elif height - xy_list[i][1] - rad < 2:
+                xy_list[i][2] = 'v' + xy_list[i][2][1]
+        pygame.display.flip()
+        screen.fill((0, 0, 0))
     pygame.quit()
