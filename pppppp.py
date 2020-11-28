0265ecbd8e31432fd2a45e9f72b5634846775c0b
Ovsiannikov Andrey <andrey22ovs@yandex.ru>
2020-11-28 22:27:45 +0300

    Перетаскивание

--- pppppp.py
+++ pppppp.py
@@ -3,46 +3,41 @@ import pygame
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Решение')
-    size = width, height = 500, 500
+    size = width, height = 300, 300
     screen = pygame.display.set_mode(size)
     screen.fill((0, 0, 0))
     fps = 95  # количество кадров в секунду
     clock = pygame.time.Clock()
-    rad = 10
-    v = 100
-    xy_list = []
+    x = 0
+    y = 0
+    xm1 = 0
+    ym1 = 0
+    xm2 = 0
+    ym2 = 0
+    a = 100
     running = True
+    drawing = False
+    color = pygame.Color('green')
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                xy_list.append([event.pos[0], event.pos[1], 'vl'])
-        time = clock.tick(fps)
-        for i in range(len(xy_list)):
-            pygame.draw.circle(screen, (255, 255, 255), (xy_list[i][0], xy_list[i][1]), rad)
-            if xy_list[i][2] == 'vl':
-                xy_list[i][0] -= v * time / 1000
-                xy_list[i][1] -= v * time / 1000
-            elif xy_list[i][2] == 'vr':
-                xy_list[i][0] += v * time / 1000
-                xy_list[i][1] -= v * time / 1000
-            elif xy_list[i][2] == 'dl':
-                xy_list[i][0] -= v * time / 1000
-                xy_list[i][1] += v * time / 1000
-            elif xy_list[i][2] == 'dr':
-                xy_list[i][0] += v * time / 1000
-                xy_list[i][1] += v * time / 1000
-
-            if xy_list[i][0] - rad < 2:
-                xy_list[i][2] = xy_list[i][2][0] + 'r'
-            elif xy_list[i][1] - rad < 2:
-                xy_list[i][2] = 'd' + xy_list[i][2][1]
-
-            elif width - xy_list[i][0] - rad < 2:
-                xy_list[i][2] = xy_list[i][2][0] + 'l'
-            elif height - xy_list[i][1] - rad < 2:
-                xy_list[i][2] = 'v' + xy_list[i][2][1]
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                xm1, ym1 = xm2, ym2 = event.pos
+                drawing = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                drawing = False
+            if event.type == pygame.MOUSEMOTION:
+                if drawing:
+                    xm2, ym2 = event.pos
+                    pygame.draw.circle(screen, pygame.Color('#ff0000'), (xm2, ym2), 10)
+                    x = x + (xm2 - xm1)
+                    y = y + (ym2 - ym1)
+                    xm1, ym1 = xm2, ym2
+        pygame.draw.rect(screen, color, (x, y, a, a))
+        clock.tick(fps)
+        if drawing:
+            pygame.draw.circle(screen, pygame.Color('#ff0000'), (xm2, ym2), 10)
         pygame.display.flip()
         screen.fill((0, 0, 0))
     pygame.quit()
