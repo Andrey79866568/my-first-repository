0f9718040ef171aea5f2b50ce0e45d2cbc89f7a6
Ovsiannikov Andrey <andrey22ovs@yandex.ru>
2020-11-28 17:11:32 +0300

    Жёлтый круг

--- pppppp.py
+++ pppppp.py
@@ -1,54 +1,36 @@
 import pygame
 
-
-w, hue = input().split()
-
-
-def draw_cube(x, y):
-    pass
-
-
 if __name__ == '__main__':
-    flag = True
-    try:
-        assert '.' not in w and '.' not in hue
-        hue = int(hue)
-        w = int(w)
-        assert 0 <= w <= 100 and not w % 4 and 0 <= hue <= 360
-    except AssertionError:
-        print('Неправильный формат ввода')
-        flag = False
-    except ValueError:
-        print('Неправильный формат ввода')
-        flag = False
-    if flag:
-        # инициализация pygame:
-        pygame.init()
-        pygame.display.set_caption('Cube')
-        # размеры окна:
-        size = width, height = 300, 300
-        # screen — холст, на котором нужно рисовать:
-        screen = pygame.display.set_mode(size)
-        screen.fill(pygame.Color('#000000'))
-        # формирование кадра:
-
-        color = pygame.Color(255, 255, 255)
-        x = width // 2 - w // (3 / 2)
-        y = height // 2 - w // (5 / 2)
-        color.hsva = (hue, 100, 50)
-        pygame.draw.polygon(screen, color, ((x + w, y + w), (x + w, y),
-                                            (x + w // (2 / 3), y - w // 2),
-                                            (x + w // (2 / 3), y + w // 2)))
-        color.hsva = (hue, 100, 75)
-        pygame.draw.rect(screen, color, (x, y, w, w))
-        color.hsva = (hue, 100, 100)
-        pygame.draw.polygon(screen, color, ((x, y), (x + w, y), (x + w // (2 / 3), y - w // 2),
-                                            (x + w // 2, y - w // 2)))
-
-        # смена (отрисовка) кадра:
-        pygame.display.flip()
-        # ожидание закрытия окна:
-        while pygame.event.wait().type != pygame.QUIT:
-            pass
-        # завершение работы:
-        pygame.quit()
+    pygame.init()
+    pygame.display.set_caption('Движущийся круг 2')
+    size = width, height = 400, 400
+    screen = pygame.display.set_mode(size)
+    screen.fill((0, 0, 255))
+    fps = 95  # количество кадров в секунду
+    v = 10
+    clock = pygame.time.Clock()
+    running = True
+    start = False
+    x, y = 0, 0
+    rad = 1
+    while running:  # главный игровой цикл
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                x, y = event.pos
+                rad = 1
+                screen.fill((0, 0, 255))
+                start = True
+        rad += v * clock.tick(fps) / 1000
+        if start:
+            pygame.draw.circle(screen, 'yellow', (x, y), rad)
+        # обработка остальных событий
+        # ...
+        # формирование кадра
+        # ...
+        pygame.display.flip()  # смена кадра
+        # изменение игрового мира
+        # ...
+        # временная задержка
+    pygame.quit()
