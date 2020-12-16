ecf75c4ef577cae0562db00aa89064e3295ddc9b
Ovsiannikov Andrey <andrey22ovs@yandex.ru>
2020-12-16 15:49:13 +0300

    Zoom

--- first.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-color = pygame.Color(128, 255, 0)
-hsv = color.hsva
-color.hsva = (hsv[0] + 90, hsv[1], hsv[2])
-print(color)
--- pppppp.py
+++ pppppp.py
@@ -1,35 +1,48 @@
 import pygame
+import csv
 
-def draw():
-    screen.fill((0, 0, 0))
-    color = pygame.Color('#ff0000ff')
-    pygame.draw.rect(screen, color, (1, 1, width - 2, height - 2))
+
+def zoom(list_of_points, alpha):
+    pygame.draw.polygon(screen, 'white', [
+        ((k[0] - width // 2) * alpha + width // 2, (k[1] - width // 2) * alpha + width // 2) for k in
+        list_of_points], 2)
 
 
 if __name__ == '__main__':
-    x, y = input().split()
-    # инициализация Pygame:
+    width = 501
+    size = width, width
+    with open('points.txt', 'r') as file1:
+        reader = csv.reader(file1, delimiter=')', quotechar='"')
+        for i in reader:
+            x = i[0][1:].split(';')
+            list_ik = [j[3:].split(';') for j in i[1:-1]]
+    list_ik = [x] + list_ik
+    for i in range(len(list_ik)):
+        for j in range(len(list_ik[i])):
+            if ',' in list_ik[i][j]:
+                list_ik[i][j] = list_ik[i][j].replace(',', '.')
+            if j == 0:
+                list_ik[i][j] = float(list_ik[i][j]) + width // 2
+            if j == 1:
+                list_ik[i][j] = -1 * float(list_ik[i][j]) + width // 2
+        list_ik[i] = tuple(list_ik[i])
     pygame.init()
-    pygame.display.set_caption('Крест')
-    # размеры окна:
-    assert '.' not in x and '.' not in y
-    try:
-        x = int(x)
-        y = int(y)
-    except ValueError:
-        raise ValueError('Это далеко не числа. Ты там прогу не попутал?!\n:)')
-    assert x < 1200 and y < 800
-    size = width, height = x, y
-    # screen — холст, на котором нужно рисовать:
+    print(list_ik)
+    pygame.display.set_caption('Решение')
     screen = pygame.display.set_mode(size)
-    # формирование кадра:
-    # команды рисования на холсте
-    draw()
-    # ...
-    # смена (отрисовка) кадра:
-    pygame.display.flip()
-    # ожидание закрытия окна:
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    # завершение работы:
+    screen.fill((0, 0, 0))
+    running = True
+    alpha = 20
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:
+                    alpha += 1
+                elif event.button == 5:
+                    alpha -= 1
+        zoom(list_ik, alpha)
+        pygame.display.flip()
+        screen.fill((0, 0, 0))
     pygame.quit()
