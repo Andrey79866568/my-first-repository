d8b339b8b061aa858f046d76d0722d6eb0ea2554
Andrey Ovsiannikov <andrey22ovs2yandex.ru>
2021-01-12 13:17:53 +0300

    Game over

--- pppppp.py
+++ pppppp.py
@@ -6,10 +6,7 @@ from random import randint
 
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
-    # если файл не существует, то выходим
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
+    assert os.path.isfile(fullname)
     image = pygame.image.load(fullname)
     if colorkey is not None:
         image = image.convert()
@@ -21,49 +18,38 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Bomb(pygame.sprite.Sprite):
+class GameOverSpr(pygame.sprite.Sprite):
     def __init__(self, *group, colorkey=None):
         super().__init__(*group)
-        self.speed = 2
-        self.image = load_image('bomb.png', colorkey)
-        self.bang = load_image('bombbym.png', colorkey)
-        w, h = self.bang.get_size()
-        x = randint((self.bang.get_width() - self.image.get_width()) // 2, 500 - w)
-        y = randint((self.bang.get_height() - self.image.get_height()) // 2, 500 - h)
-        self.rect = pygame.rect.Rect(x, y, *self.image.get_size())
+        self.speed = 1
+        self.image = load_image('game_over.png', colorkey)
+        self.image = pygame.transform.scale(self.image, (500, 300))
+        self.rect = pygame.rect.Rect(-500, 0, 500, 300)
 
-    def in_bbomb(self, x, y):
-        return self.rect.x < x < self.rect.x + self.rect.w and \
-               self.rect.y < y < self.rect.y + self.rect.h
-
-    def update(self, x, y):
-        if self.in_bbomb(x, y) and self.image != self.bang:
-            self.rect.x -= (self.bang.get_width() - self.rect.w) // 2
-            self.rect.y -= (self.bang.get_height() - self.rect.h) // 2
-            self.image = self.bang
+    def update(self, x_i, time):
+        if x_i - (self.rect.x + self.rect.w) > 2:
+            self.rect.x += self.speed * time
+        else:
+            self.rect.x = x_i - self.rect.w
 
 
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Решение')
-    size = width, height = 500, 500
+    size = width, height = 500, 300
     screen = pygame.display.set_mode(size)
-    screen.fill((0, 0, 0))
-    all_sprite = pygame.sprite.Group()
-    for i in range(20):
-        Bomb(all_sprite)
-    fps = 60
+    screen.fill((0, 0, 255))
+    all_sprites = pygame.sprite.Group()
+    fps = 500
+    gg = GameOverSpr(all_sprites)
     clock = pygame.time.Clock()
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    all_sprite.update(*event.pos)
-        all_sprite.draw(screen)
-        clock.tick(fps)
+        all_sprites.update(width, clock.tick(fps))
+        all_sprites.draw(screen)
         pygame.display.flip()
-        screen.fill((0, 0, 0))
+        screen.fill((0, 0, 255))
     pygame.quit()
