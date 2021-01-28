4bfd418be984b0cdadfdac50ddd3eafebd6389f7
Andrey Ovsiannikov <andrey22ovs2yandex.ru>
2021-01-28 22:09:47 +0300

    часы с кукушкой

--- pppppp.py
+++ pppppp.py
@@ -1,10 +1,19 @@
 #!C:\Users\ovsia\PycharmProjects\andreysrepo\venv\Scripts\python.exe
 
-import sys
+import schedule
+import datetime as dt
 
-try:
-    assert len(sys.argv) == 3
-    x, y = int(sys.argv[1]), int(sys.argv[2])
-    print(x + y)
-except (IndexError, AssertionError, ValueError):
-    print(0)
+
+def job():
+    flag = True
+    if dt.datetime.now().second + dt.datetime.now().minute == 0 and flag:
+        print('Ку')
+        flag = False
+    elif not dt.datetime.now().minute + dt.datetime.now().second == 0:
+        flag = True
+
+
+schedule.every(1).seconds.do(job)
+
+while True:
+    schedule.run_pending()
