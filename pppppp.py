2b6906fde4c5d8a66f01dc69b13df890773970e3
Andrey Ovsiannikov <andrey22ovs2yandex.ru>
2021-01-29 20:57:14 +0300

    словарь параметров

--- pppppp.py
+++ pppppp.py
@@ -1,19 +1,11 @@
 #!C:\Users\ovsia\PycharmProjects\andreysrepo\venv\Scripts\python.exe
 
-import schedule
-import datetime as dt
+import sys
 
-
-def job():
-    flag = True
-    if dt.datetime.now().second + dt.datetime.now().minute == 0 and flag:
-        print('Ку')
-        flag = False
-    elif not dt.datetime.now().minute + dt.datetime.now().second == 0:
-        flag = True
-
-
-schedule.every(1).seconds.do(job)
-
-while True:
-    schedule.run_pending()
+dict_of_values = {i.split('=')[0]: i.split('=')[1] for i in sys.argv[1:] if i != '--sort'}
+if '--sort' in sys.argv:
+    dict_of_values = sorted(dict_of_values.items())
+else:
+    dict_of_values = dict_of_values.items()
+for key, elem in dict_of_values:
+    print(f'Key: {key} Value: {elem}')
