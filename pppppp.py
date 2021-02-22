a8dc067d951cc3e902d2b3d1b83dad5f2892aca6
ovad4221 <andrey22ovs@yandex.ru>
2021-02-22 18:28:08 +0300

    Колонизация Марса

--- pppppp.py
+++ pppppp.py
@@ -1,25 +1,17 @@
-#!C:\Users\ovsia\PycharmProjects\andreysrepo\venv\Scripts\python.exe
-import sys
-import argparse
-# test commit
+from flask import Flask
 
-parser = argparse.ArgumentParser()
-parser.add_argument('file_from')
-parser.add_argument('file_to')
-parser.add_argument('--upper', action='store_true', default=False, dest='up_text')
-parser.add_argument('--lines', type=int, default=-1)
-args = parser.parse_args()
-try:
-    with open(args.file_from, 'r') as file_from:
-        lines = file_from.readlines()
-        n = args.lines
-        if n == -1 or n > len(lines):
-            n = len(lines)
-        with open(args.file_to, 'w') as file_to:
-            for i in range(n):
-                if args.up_text:
-                    file_to.write(''.join([j.upper() for j in lines[i]]))
-                else:
-                    file_to.write(lines[i])
-except FileNotFoundError:
-    print('error')
+app = Flask(__name__)
+
+
+@app.route('/')
+def start():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def deviz():
+    return 'И на Марсе будут яблони цвести!'
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
