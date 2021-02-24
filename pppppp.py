dc5931f9e409b2462e34d95921f0db51278a7d09
ovad4221 <andrey22ovs@yandex.ru>
2021-02-24 17:37:01 +0300

    Изображение Марса

--- pppppp.py
+++ pppppp.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, url_for
+import os
 
 app = Flask(__name__)
 
@@ -11,6 +12,21 @@ def start():
 @app.route('/index')
 def deviz():
     return 'И на Марсе будут яблони цвести!'
+
+
+@app.route('/promotion')
+def promotion():
+    list_ik = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+               'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
+    return '</br>'.join(list_ik)
+
+
+@app.route('/image_sample')
+def image():
+    return f'''<h1>Жди нас, Марс</h1>
+           <img src="{url_for('static', filename='images/Mars.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
+           дак вот ты какая, красная планета'''
 
 
 if __name__ == '__main__':
