b6a0892c6be86c7c1108286f74e940c1dd96a8fc
ovad4221 <andrey22ovs@yandex.ru>
2021-03-02 23:35:08 +0300

    Загрузка файла

--- pppppp.py
+++ pppppp.py
@@ -1,37 +1,47 @@
 from flask import Flask, url_for, request
+import os
 
 app = Flask(__name__)
 
 
-@app.route('/results/<nickname>/<int:level>/<float:rating>')
-def result(nickname, level, rating):
-    return f'''<!doctype html>
-                <html lang="en">
-                    <head>
-                    <meta charset="utf-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                    crossorigin="anonymous">
-                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                    <title>Привет!</title>
-                  </head>
-                  <body>
-                    <h1 align="center" style="color:#000000">Результаты отбора</h1>
-                    <h3 align="center" style="color:#ffcc00">Претендента на участие в миссии {nickname}<h3>
-                    <div class="alert alert-danger" role="alert">
-                        Поздравляем! Ваш рейтинг после {level} этапа отбора
-                    </div>
-                    <div class="alert alert-warning" role="alert">
-                        составляет {rating}!
-                    </div>
-                    <div class="alert alert-success" role="alert">
-                        Желаем удачи!
-                    </div>
-                  </body>
-                </html>
-                    '''
+@app.route('/sample_file_upload', methods=['POST', 'GET'])
+def sample_file_upload():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                             <link rel="stylesheet"
+                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                             crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Пример загрузки файла</title>
+                          </head>
+                          <body>
+                            <h1 align="center" style="color:#000000">Загрузка фотографии</h1>
+                            <h3 align="center" style="color:#000000">для участия в миссии </h3>
+                            <div>
+                                <form class="login_form" method="post" enctype="multipart/form-data">
+                                   <div class="form-group">
+                                        <label for="photo">Приложите фотографию.</label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                        <label>~~~~~~~~~~~~~~~~~~~~~~~~</label>
+                                        <img src="{url_for('static', filename='images/robot.jpg')}" 
+                                            alt="здесь должна была быть картинка, но не нашлась">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        f = request.files['file']
+        with open(os.path.join('static', 'images',  'request_image.jpg'), 'wb') as file:
+            for i in f.readlines():
+                file.write(i)
+        return f'''<img src="{url_for('static', filename='images/request_image.jpg')}">'''
 
 
 if __name__ == '__main__':
