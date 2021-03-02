99e62fc7b056c53e643761f48cabdcf073edbd19
ovad4221 <andrey22ovs@yandex.ru>
2021-03-03 00:02:35 +0300

    Nedocarusel

--- pppppp.py
+++ pppppp.py
@@ -4,10 +4,9 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/sample_file_upload', methods=['POST', 'GET'])
-def sample_file_upload():
-    if request.method == 'GET':
-        return f'''<!doctype html>
+@app.route('/carusel')
+def galery():
+    return f'''<!doctype html>
                         <html lang="en">
                           <head>
                             <meta charset="utf-8">
@@ -16,32 +15,38 @@ def sample_file_upload():
                              href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
                              integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
                              crossorigin="anonymous">
-                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}"/>
                             <title>Пример загрузки файла</title>
                           </head>
                           <body>
-                            <h1 align="center" style="color:#000000">Загрузка фотографии</h1>
-                            <h3 align="center" style="color:#000000">для участия в миссии </h3>
-                            <div>
-                                <form class="login_form" method="post" enctype="multipart/form-data">
-                                   <div class="form-group">
-                                        <label for="photo">Приложите фотографию.</label>
-                                        <input type="file" class="form-control-file" id="photo" name="file">
-                                        <label>~~~~~~~~~~~~~~~~~~~~~~~~</label>
-                                        <img src="{url_for('static', filename='images/robot.jpg')}" 
-                                            alt="здесь должна была быть картинка, но не нашлась">
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Отправить</button>
-                                </form>
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                              <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                              </ol>
+                              <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                  <img class="d-block w-100" src="{url_for('static', filename='images/hello.jpg')}" alt="First slide">
+                                </div>
+                                <div class="carousel-item">
+                                  <img class="d-block w-100" src="{url_for('static', filename='images/howareyou.jpg')}" alt="Second slide">
+                                </div>
+                                <div class="carousel-item">
+                                  <img class="d-block w-100" src="{url_for('static', filename='images/by.jpg')}" alt="Third slide">
+                                </div>
+                              </div>
+                              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                              </a>
+                              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                              </a>
                             </div>
                           </body>
                         </html>'''
-    elif request.method == 'POST':
-        f = request.files['file']
-        with open(os.path.join('static', 'images',  'request_image.jpg'), 'wb') as file:
-            for i in f.readlines():
-                file.write(i)
-        return f'''<img src="{url_for('static', filename='images/request_image.jpg')}">'''
 
 
 if __name__ == '__main__':
