f732b717de14359831bdae790c675b066acfbf25
ovad4221 <andrey22ovs@yandex.ru>
2021-03-02 19:46:39 +0300

    не помню о чем комит

--- pppppp.py
+++ pppppp.py
@@ -1,12 +1,19 @@
 from flask import Flask, url_for, request
-import os
 
 app = Flask(__name__)
 list_ik = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
            'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
 
 
-@app.route('/form_sample', methods=['POST', 'GET'])
+@app.route('/image_mars')
+def image():
+    return f'''<h1>Жди нас, Марс</h1>
+           <img src="{url_for('static', filename='images/Mars.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
+           дак вот ты какая, красная планета'''
+
+
+@app.route('/astronaut_selection', methods=['POST', 'GET'])
 def form_sample():
     if request.method == 'GET':
         return f'''<!doctype html>
@@ -122,22 +129,22 @@ def promotion():
 
 
 @app.route('/promotion_image')
-def image():
+def image_prom():
     return f'''<!doctype html>
                 <html lang="en">
                     <head>
                     <meta charset="utf-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                    <link rel="stylesheet" 
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
-                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    <link rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
                     crossorigin="anonymous">
                     <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
                     <title>Привет, Яндекс!</title>
                   </head>
                   <body>
                     <h1>Жди нас, Марс</h1>
-                    <img src="{url_for('static', filename='images/Mars.jpg')}" 
+                    <img src="{url_for('static', filename='images/Mars.jpg')}"
                     alt="здесь должна была быть картинка, но не нашлась">
                     <div class="alert alert-danger" role="alert">
                         {list_ik[0]}
