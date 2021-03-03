1f674d93265da92713de22472ec3152a931b94b4
ovad4221 <andrey22ovs@yandex.ru>
2021-03-03 13:18:28 +0300

    Готовимся к миссии

--- pppppp.py
+++ pppppp.py
@@ -1,52 +1,28 @@
-from flask import Flask, url_for, request
-import os
+from flask import Flask, url_for, request, render_template
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/carusel')
-def galery():
-    return f'''<!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                             <link rel="stylesheet"
-                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                             crossorigin="anonymous">
-                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}"/>
-                            <title>Пример загрузки файла</title>
-                          </head>
-                          <body>
-                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                              <ol class="carousel-indicators">
-                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                              </ol>
-                              <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                  <img class="d-block w-100" src="{url_for('static', filename='images/hello.jpg')}" alt="First slide">
-                                </div>
-                                <div class="carousel-item">
-                                  <img class="d-block w-100" src="{url_for('static', filename='images/howareyou.jpg')}" alt="Second slide">
-                                </div>
-                                <div class="carousel-item">
-                                  <img class="d-block w-100" src="{url_for('static', filename='images/by.jpg')}" alt="Third slide">
-                                </div>
-                              </div>
-                              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                              </a>
-                              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                              </a>
-                            </div>
-                          </body>
-                        </html>'''
+@app.route('/')
+@app.route('/index')
+def index():
+    params = dict()
+    params['title'] = 'Заготовка'
+    return render_template('base.html', **params)
+
+
+@app.route('/odd_even')
+def odd_even():
+    return render_template('odd_even.html', number=11)
+
+
+@app.route('/news')
+def news():
+    with open("news.json", "rt", encoding="utf8") as f:
+        news_list = json.loads(f.read())
+    print(news_list)
+    return render_template('news.html', news=news_list)
 
 
 if __name__ == '__main__':
