5bb50affc1f5de712548959ea8c41878709884c5
ovad4221 <andrey22ovs@yandex.ru>
2021-03-08 15:54:41 +0300

    Автоматический ответ

--- my_site.py
+++ my_site.py
@@ -2,27 +2,38 @@ from flask import Flask, url_for, request, render_template
 import json
 
 app = Flask(__name__)
+params = dict()
+params['title'] = 'Заготовка'
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    params = dict()
-    params['title'] = 'Заготовка'
     return render_template('base_nav.html', **params)
 
 
 @app.route('/training/<prof>')
 def training(prof):
-    params = dict()
     params['name'] = prof
-    params['title'] = 'Заготовка'
     return render_template('tren.html', **params)
 
 
 @app.route('/list_prof/<type_of_list>')
 def list_view(type_of_list):
     return render_template('list_prof.html', type=type_of_list)
+
+
+@app.route('/answer')
+def answer():
+    params['title'] = 'Анкета-ответ'
+    params['surname'] = 'Watny'
+    params['name'] = 'Mark'
+    params['education'] = 'выше среднего'
+    params['profession'] = 'Штурман марсохода'
+    params['sex'] = 'male'
+    params['motivation'] = 'Всегда мечтал застрять на Марсе!'
+    params['ready'] = True
+    return render_template('avto_answer.html', **params)
 
 
 if __name__ == '__main__':
