e03f804aeaec934f953b9300dcedf849448e2003
ovad4221 <andrey22ovs@yandex.ru>
2021-03-08 23:52:27 +0300

    Двойная защита

--- my_site.py
+++ my_site.py
@@ -1,9 +1,45 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 import json
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '#Mars$%Colonisation$%Secret$%Key!!!'
 params = dict()
-params['title'] = 'Заготовка'
+params['title'] = 'Our Mission'
+params['profession'] = 'anonymous'
+params['style'] = '/static/css/style.css'
+params_answer = dict()
+params_answer['style'] = '/static/css/style.css'
+
+
+class AlertForm(FlaskForm):
+    user1 = StringField('Логин', validators=[DataRequired()])
+    password1 = PasswordField('Пароль', validators=[DataRequired()])
+    user2 = StringField('Логин', validators=[DataRequired()])
+    password2 = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('СТАРТ')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_astr():
+    if request.method == 'GET':
+        return render_template('login_astr.html')
+    elif request.method == 'POST':
+        params_answer['title'] = 'Анкета-ответ'
+        params_answer['surname'] = request.form['surname']
+        params_answer['name'] = request.form['name']
+        params_answer['education'] = request.form['class']
+        params_answer['profession'] = request.form['profs']
+        params_answer['sex'] = request.form['sex']
+        params_answer['motivation'] = request.form['about']
+        if 'accept_to_stay' in request.form:
+            params_answer['ready'] = request.form['accept_to_stay']
+        else:
+            params_answer['ready'] = 'off'
+        params['profession'] = params_answer['profession']
+        return answer()
 
 
 @app.route('/')
@@ -14,26 +50,37 @@ def index():
 
 @app.route('/training/<prof>')
 def training(prof):
-    params['name'] = prof
+    params['name_prof'] = prof
     return render_template('tren.html', **params)
 
 
 @app.route('/list_prof/<type_of_list>')
 def list_view(type_of_list):
-    return render_template('list_prof.html', type=type_of_list)
+    params['type'] = type_of_list
+    return render_template('list_prof.html', **params)
 
 
 @app.route('/answer')
 def answer():
-    params['title'] = 'Анкета-ответ'
-    params['surname'] = 'Watny'
-    params['name'] = 'Mark'
-    params['education'] = 'выше среднего'
-    params['profession'] = 'Штурман марсохода'
-    params['sex'] = 'male'
-    params['motivation'] = 'Всегда мечтал застрять на Марсе!'
-    params['ready'] = True
-    return render_template('avto_answer.html', **params)
+    return render_template('avto_answer.html', **params_answer)
+
+
+# @app.route('/login_p', methods=['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         return redirect('/success')
+#     return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/alert')
+def alert():
+    params_alert = params.copy()
+    params_alert['style'] = '/static/css/alert_style.css'
+    form = AlertForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('alert.html', form=form, **params_alert)
 
 
 if __name__ == '__main__':
--- pppppp.py
+++ pppppp.py
@@ -1,1 +1,30 @@
 # теперь это вспомогательный файл
+from flask import Flask, url_for, request, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '#Mars$%Colonisation$%Secret$%Key!!!'
+params = dict()
+params['title'] = 'Our Mission'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password1 = PasswordField('Повторите пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
